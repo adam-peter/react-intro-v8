3ea11178680d347bc061146597b76327154fd4c3
badmood111 <petrzela.adam@seznam.cz>
2023-01-01 14:04:27 +0100

    Refactor Carousel

---
+++ src/Carousel.tsx
@@ -0,0 +1,56 @@
+import { Component, MouseEvent } from "react";
+
+interface IProps {
+  images: string[];
+}
+
+class Carousel extends Component<IProps> {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (event: MouseEvent<HTMLElement>) => {
+    if (!(event.target instanceof HTMLElement)){
+      return;
+    }
+
+    if (event.target.dataset.index){
+      this.setState({
+        active: +event.target.dataset.index,
+      });
+    }
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    return (
+      <div className="mx-auto">
+        <img
+          src={images[active]}
+          alt="animal"
+          className="mx-auto h-56 rounded-lg"
+        />
+        <div className="grid grid-cols-1 justify-items-center sm:grid-cols-2">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "border-4 border-red-400" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
